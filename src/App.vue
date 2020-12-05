8a48848efbc23b68a4b4c2963626de8ea6d33e72
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-12-05 06:19:34 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.title" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "VuePic",
+      fotos: [
+        {
+          url:
+            "https://tabelacfop.com.br/assets/images/LogomarcaConectaTecnologia_v1.0-tts.png",
+          title: "Conecta Tecnologia"
+        },
+        {
+          url:
+            "https://tabelacfop.com.br/assets/images/LogomarcaConectaTecnologia_v1.0-tts.png",
+          title: "Conecta Tecnologia"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
