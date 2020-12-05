08eab90d81dec1338327f48658f3a785f8368526
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-12-05 06:41:39 -0300

    add style and rename variables

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-    <h1>{{ title }}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.title" />
+  <div id="app" class="body">
+    <h1 class="title">{{ title }}</h1>
+    <ul class="photos-list">
+      <li class="photos-item" v-for="photo of photos">
+        <img :src="photo.url" :alt="photo.title" />
       </li>
     </ul>
   </div>
@@ -14,7 +14,7 @@ export default {
   data() {
     return {
       title: "VuePic",
-      fotos: []
+      photos: []
     };
   },
   created() {
@@ -22,10 +22,27 @@ export default {
 
     promise
     .then(res => res.json())
-    .then(fotos => (this.fotos = fotos), err => console.log(err));
+    .then(photos => (this.photos = photos), err => console.log(err));
   }
 };
 </script>
 
 <style>
+.body{
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
+.title{
+  text-align: center;
+}
+
+.photos-list{
+  list-style: none;
+}
+
+.photos-list .photos-item{
+display: inline-block;
+}
+
 </style>
