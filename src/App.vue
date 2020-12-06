7257ee5b93df2325984704c5835659303f0647cf
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-12-06 11:15:48 -0300

    add menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,27 @@
 <template>
   <div id="app" class="body">
+    <nav>
+      <ul>
+        <li v-for="route in routes">
+          <router-link :to="route.path ? route.path : '/'">
+            {{ route.title }}
+          </router-link>
+        </li>
+      </ul>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { routes } from "./routes";
+export default {
+  data() {
+    return {
+      routes,
+    };
+  },
+};
 </script>
 
 <style>
