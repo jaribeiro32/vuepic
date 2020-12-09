a9012f7d0fc6e575c5aa5fdaa00ed81696116872
Jhonny Ribeiro <jaribeiro32@gmail.com>
2020-12-09 02:06:55 -0300

    outra forma de importar tranform

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -33,12 +33,18 @@
 import Painel from "../shared/painel/Painel.vue";
 import ImagemResponsiva from "../shared/imagem-responsiva/imagem-responsiva.vue";
 import Botao from "../shared/botao/Botao.vue";
+import transform from "../../directives/Tranform";
 export default {
   components: {
     "meu-painel": Painel,
     "imagem-responsiva": ImagemResponsiva,
     "meu-botao": Botao,
   },
+
+  directives: {
+    "meu-transform": transform,
+  },
+
   data() {
     return {
       title: "VuePic",
--- src/directives/Tranform.js
+++ src/directives/Tranform.js
@@ -1,30 +1,31 @@
-import Vue from "vue";
+export default{
 
-Vue.directive("meu-transform", {
-  bind(el, binding, vnode) {
-    let current = 0;
-
-    el.addEventListener("dblclick", function() {
-      let incremento = binding.value || 90;
-
-      let efeito;
-
-      if (!binding.arg || binding.arg == "rotate") {
-        if (binding.modifiers.reverse) {
-          current -= incremento;
-        } else {
-          current += incremento;
-        }
-
-        efeito = `rotate(${current}deg)`;
-      } else if (binding.arg == "scale") {
-        efeito = `scale(${incremento})`;
+    bind(el, binding, vnode) {
+        let current = 0;
+    
+        el.addEventListener("dblclick", function() {
+          let incremento = binding.value || 90;
+    
+          let efeito;
+    
+          if (!binding.arg || binding.arg == "rotate") {
+            if (binding.modifiers.reverse) {
+              current -= incremento;
+            } else {
+              current += incremento;
+            }
+    
+            efeito = `rotate(${current}deg)`;
+          } else if (binding.arg == "scale") {
+            efeito = `scale(${incremento})`;
+          }
+    
+          el.style.transform = efeito;
+    
+          if (binding.modifiers.animate) el.style.transition = "transform 0.5s";
+    
+        });
       }
+        
+}
 
-      el.style.transform = efeito;
-
-      if (binding.modifiers.animate) el.style.transition = "transform 0.5s";
-
-    });
-  }
-});
